c9d2a2b47f73a5a0b41793f7dbdd24d0c317daad
sidhxrt <sidharthh1880@gmail.com>
2024-10-23 00:36:44 +0530

    eucldn dist & cos dist added

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from flask import Flask, request
-from vectormath import cosine_sim
+from vectormath import cosine_sim, cosine_dist, euclidean_dist
 app = Flask(__name__)
 
 @app.route('/')
 def greet():
     return 'working on trigonometry rn'
 
+@app.route('/cosinedist', methods=['POST'])
+def cosinedist():
+# accepts 2 vectors x and y from user
+    if request.method == 'POST':
+        if request.is_json:
+            json_data = request.get_json()
+            x = json_data.get('vector1')
+            y = json_data.get('vector2')
+            return cosine_dist(x, y)
+        return "400, only json accepted"
+    return "invalid http method"
+
 @app.route('/cosinesim', methods=['POST'])
 def cosinesim():
+# accepts 2 vectors x and y from user
     if request.method == 'POST':
         if request.is_json:
             json_data = request.get_json()
@@ -17,5 +30,20 @@ def cosinesim():
         return "400, only json accepted"
     return "invalid http method"
 
+@app.route('/euclideandist', methods=['POST'])
+def cosinedist():
+# accepts 2 vectors x and y from user
+    if request.method == 'POST':
+        if request.is_json:
+            json_data = request.get_json()
+            x = json_data.get('vector1')
+            y = json_data.get('vector2')
+            return euclidean_dist(x, y)
+        return "400, only json accepted"
+    return "invalid http method"
+
 if __name__ == '__main__':
     app.run()
+
+# supports only if len(x) == len(y)
+# class needs to be implemented
--- vectormath.py
+++ vectormath.py
@@ -1,4 +1,4 @@
-def cosinesim(x, y):
+def cosine_sim(x, y):
     if(len(x) == len(y)):
         sum = 0
         xsq = 0
@@ -10,3 +10,13 @@ def cosinesim(x, y):
         X = xsq ** 0.5
         Y = ysq ** 0.5
         return sum/(X*Y)
+    
+def cosine_dist(x, y):
+    return 1 - cosine_sim(x, y)
+    
+def euclidean_dist(x, y):
+    if(len(x) == len(y)):
+        sum = 0
+        for i in range(len(x)):
+           sum = sum + ((x[i] - y[i])**2)
+        return sum**0.5 
